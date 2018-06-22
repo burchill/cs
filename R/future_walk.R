048f2b719f3eb00ebfca08e9b3fab7077e416e57
burchill <zach.burchill+gh@gmail.com>
2018-06-22 13:09:40 -0400

    first commit

---
+++ R/future_walk.R
@@ -0,0 +1,113 @@
+#' Wrap a function with `collect_all`
+#' 
+#' This takes a function, and returns a new one, but makes it so that it goes from f(x) to collect_all(f(x)(x)). Ehhh... look at the code.
+#' 
+#' @param f a function
+#' @export
+collect_decorator <- function(f) {
+  f <- purrr::as_mapper(f)
+  newfie <- function() x
+  formals(newfie) <- formals(f)
+  bod <- body(f)
+  body(newfie) <- substitute(zplyr::collect_all(bod, catchErrors = TRUE)[2:4])
+  return(newfie)
+}
+
+#' Require a certain package to be installed 
+#' 
+#' Eh, just look at the code
+#' 
+#' @param package a string of the package name
+#' @param install_location a string appended to the end of the warning
+#' @export
+warn_about_package <- function(package, install_location=".") {
+  if (!rlang::is_installed(package)) {
+    m <- paste0("The `", package, "` is required for what you're doing here. Please install it", install_location)
+    stop(m, call.=TRUE)
+  }
+}
+
+
+
+#' Make your own versions of the `furrr` `future_map` functions
+#' 
+#' This function is responsible for making the \code{\link{future_walk}} set of functions. The code used to make it is pretty helpful for understanding \R's meta-programming, and if you need to modify the input function `.f` to the \code{\link[furrr]{future_map}} functions, you can change the `decorator_func` argument to whatever you want.
+#' 
+#' I wanted functions that would behave with all the complexity of `furrr`'s \code{\link[furrr]{future_map}} functions (which, due to this complexity, are mostly a blackbox to me), while modifying the behavior of the specified input function, basically so I wouldn't have to keep on adding `zplyr::collect_all` to these argument functions, like: `furrr::future_map(c(1:4), ~zplyr::collect_all(saveRDS(...)))`. I'm inordinately proud of what I did here, making a function that would be able to take \emph{any} of the `future_map` functions and turn them into what I had in mind. Ooooh lawdy, I loved learning this.
+#' 
+#' @param .orig_function the specific `future_map`-like function you want to base yours off of
+#' @param decorator_func the function that you want to use to modify the given `.f` argument I use the term "decorator" like this is analogous to Python, but it's not 100\% accurate
+#' @seealso \code{\link{collect_decorator}}, \code{\link{warn_about_package}}
+#' @rdname future_walk_maker
+#' @export
+.future_walk_maker <- function(.orig_function,
+                               decorator_func = collect_decorator) {
+  warn_about_packages <- function() {
+    warn_about_package("furrr")
+    warn_about_package("zplyr", 
+                       " with `devtools::install_github(\"burchill/zplyr\")`.")
+  }
+  
+  warn_about_package("furrr")
+  
+  # get_the_secret_shit <- function(p) {
+  #   dp <- deparse(p)
+  #   dp[2] <- paste0("furrr:::", gsub("^\\s+","", dp[2]))
+  #   return(parse(text=dp, n=1)[[1]])
+  # }
+  
+  new_function <- function() x
+  formals(new_function) <- formals(.orig_function)
+  # orig_bod <- get_the_secret_shit(body(.orig_function))
+  orig_bod <- body(.orig_function)
+  new_bod <- substitute({ warn_about_packages(); .f <- decorator_func(.f); orig_bod })
+  body(new_function) <- new_bod
+  environment(new_function) <- asNamespace('furrr')
+  return(new_function)
+}
+
+
+
+#' Supply each element in a vector to a function and collect the messages, warnings, and errors
+#'
+#' Think of these functions as the `walk` analogues to `furrr`'s \code{\link[furrr]{future_map} }
+#' functions. However, unlike `purrr::walk` and its ilk, these functions will 
+#' not return the same value, but will return a list of named lists, where 
+#' each named list contains a sublist of messages, warnings, and errors that 
+#' arose during the execution of the function.  \cr \cr
+#' These functions are basically designed to be used when instead of returning 
+#' data remotely, you want to save it somewhere. The (relatively) light-weight 
+#' list they'll return will help you diagnose what went wrong, etc.
+#' 
+#' Underlyingly, these functions call the `furrr` `future_map` functions, but take the `.f` argument and essentially edit it, squeezing in a `zplyr::collect_all` wrapper while removing the `$value` element (this was done because accidentally returning huge models would slow down the parallelization considerably).
+#'
+#' @inheritParams furrr::future_map
+#' @inheritParams furrr::future_pmap
+#' @inheritParams furrr::future_map2
+#'
+#' @seealso \code{\link{collected_info_to_df}}, \code{\link{.future_walk_maker}}
+#' @rdname walk_functions
+#' @export
+future_walk <- .future_walk_maker(furrr::future_map)
+#' @rdname walk_functions
+#' @export
+future_walk2 <- .future_walk_maker(furrr::future_map2)
+#' @rdname walk_functions
+#' @export
+future_pwalk <- .future_walk_maker(furrr::future_pmap)
+
+#' Turn a list of information about a list of calls into a data frame
+#' 
+#' The output of the \code{\link{future_walk}} functions is naturally in a list of lists. This function will turn it into a `tbl_df` with each column corresponding to the errors, messages, and warnings. If the list is named (i.e., the input list to \code{\link{future_walk}} was named), then there will also be a column `name` which will contain these values. Columns that had no returned information will default to `NA` and cells that did not have any returned values will default to `<NULL>`.
+#' 
+#' @param .l The list of collected information
+#' @return a `tbl_df` data frame
+#' @seealso \code{\link{future_walk}}
+#' @export
+collected_info_to_df <- function(.l) {
+  some_names <- !is.null(names(.l)) & (FALSE %in% (names(.l)== ""))
+  df <- purrr::modify_depth(.l, 2, ~ifelse(length(.) < 1, NA, list(.))) %>%
+    purrr::map_dfr(~tibble:as_tibble(.x))
+  if (some_names) df %>% mutate(names = names(.l))
+  else df
+}
---
+++ R/plans.R
@@ -0,0 +1,372 @@
+.need_future <- function() {
+  if (!requireNamespace("future", quietly = TRUE)) {
+    stop("Package \"future\" needed for this function to work / work with these parameters. Please install it.",
+         call. = FALSE)
+  }
+}
+
+
+#' Assign futures from different levels of the topology
+#'
+#' Let's say you want to run a job on the bcs-cycle1 server, which
+#' would require doing two nested future plans. Instead of having to
+#' type out something like: \code{x \%<-\% { y \%<-\% { 1 + 1 }; y}}, you can
+#' now just type \code{x \%2\% { 1 + 1 }}, which will run \code{1 + 1} on the
+#' second layer of the future topology.  Likewise, \code{\%3\%} will assign
+#' the expression when evaluated on the third level.
+#'
+#' @rdname nested_pipe
+#' @export
+`%2%` <- function(x, value) {
+  target <- substitute(x)
+  inner_expr <- substitute(value)
+  expr <- substitute({.zach %<-% inner_expr; .zach })
+  envir <- parent.frame(1)
+  future:::futureAssignInternal(target, expr, envir = envir, substitute = FALSE)
+}
+
+#' @rdname nested_pipe
+#' @export
+`%3%` <- function(x, value) {
+  target <- substitute(x)
+  inner_expr <- substitute(value)
+  expr <- substitute({.zach %<-% {.is_da_best %<-% inner_expr; .is_da_best}; .zach })
+  envir <- parent.frame(1)
+  future:::futureAssignInternal(target, expr, envir = envir, substitute = FALSE)
+}
+
+
+#' The default way to make multiprocess plans for the bcs-cycle1 server
+#'
+#' If one wants to run jobs on the bcs-cycle1 server.
+#'
+#' @param login_node a string for the gateway login, e.g., \"zachburchill@cycle1.cs.rochester.edu\"
+#' @param core_function the function determining the number of cores to use on the server. By default, it is the default of `multiprocess`. You can specify a custom function or just set a number yourself
+#' @param backoff_threshold the threshold of percentage of available memory below which the connection is cancelled
+#' @export
+bcs_planner <- function(login_node,
+                        core_function = future::availableCores,
+                        backoff_threshold = 0.7) {
+  .need_future()
+  core_function_expr <- quo_name(enquo(core_function))
+
+  # Make it callable if it's, say, a number
+  if (!rlang::is_callable(core_function)) {
+    temp_val <- core_function
+    core_function <- function() return(temp_val)
+  } else {
+    core_function_expr <- paste0(core_function_expr, "()")
+  }
+
+  # original plan
+  oplan <- plan()
+
+  message_string <- paste0("plan(list(\n  tweak(remote, workers = \"", login_node, "\"),",
+                           "\n  tweak(remote, workers = \"bcs-cycle1.cs.rochester.edu\"),",
+                           "\n  tweak(multiprocess, workers = ", core_function_expr, ")\n))")
+  message(paste0("Running:\n", message_string))
+
+
+
+  plan(list(
+    tweak(remote, workers = login_node),
+    tweak(remote, workers = "bcs-cycle1.cs.rochester.edu"),
+    tweak(multiprocess, workers = core_function())
+  ))
+
+  server_detes %<-% {
+    y %<-% {
+      system("free", intern = TRUE) %>%
+        paste0(collapse="") %>%
+        { gsub("\\s+", " ", .) } %>%
+        stringr::str_match("(?<=Mem: )[0-9]+ [0-9]+") %>%
+        purrr::pluck(1) %>% {
+          vals <- as.numeric(strsplit(., " ")[[1]])
+          1 - (vals[2]/vals[1])
+        } %>%
+        c(availableCores(), core_function())
+    }
+    y
+  }
+  free_mem <- server_detes[1]
+  avail <- server_detes[2]
+  using <- server_detes[3]
+  message(paste0(round(free_mem*100), "% of memory available on server. ",
+                 avail, " cores available, using: ", using))
+  if (free_mem < backoff_threshold) {
+    on.exit(plan(oplan), add = TRUE)
+    stop(paste0("% of memory available less than ",
+                       round(backoff_threshold*100), ". Backing off and reverting to previous plan"))
+  }
+  return_null <- NULL
+}
+
+
+#' The default way to start a cluster plan
+#'
+#' Runs `future`'s plan, in a way that works well with the Rochester cluster nodes
+#'
+#' @param login_node a string for the gateway login, e.g., \"zachburchill@cycle1.cs.rochester.edu\"
+#' @param n the number of clusters to use
+#' @param \dots additional bare arguments for `filter()`
+#' @param use_abbreviations because the way ssh adds known hosts, if you've only sshed into a node via `ssh nodeN`, you'll want to set this to `TRUE`
+#' @return By default, nothing, but possibly an expression if `goahead=FALSE`
+#' @export
+default_planner <- function(login_node, n,
+                            ...,
+                            use_abbreviations = TRUE) {
+  .need_future()
+
+  filterers <- rlang::enquos(...)
+
+
+  # Get the node information
+  node_df <- get_nodes_info(login_node)
+  node_df %>%
+    # Make the data frame clean
+    default_cleanup() %>%
+    # Filter out the ones that are down/overused
+    default_filter() %>%
+    # Presumably, we only get to use nodes 33-64...
+    filter(number < 65) %>%
+    # Custom filters
+    filter(!!! filterers) %>%
+    # Pick the n best
+    get_n_best_nodes(n) %>%
+    # Turn the nodes into the plan
+    nodes_to_plan(login_node, use_abbreviations = TRUE)
+}
+
+
+
+#' Get the status of all the nodes
+#'
+#' Finds the statuses of all the nodes in the cluster via logging in and
+#' running `pbsnodes` on one of the nodes, and then parsing that information
+#' into a data frame.
+#'
+#' @param login_node a string for the gateway login, e.g., \"zachburchill@cycle1.cs.rochester.edu\"
+#' @param check_node the name of any "nodeN" hostname, to get the data from
+#' @return A data frame with all the node information
+#' @export
+get_nodes_info <- function(login_node,
+                           check_node="node64") {
+  .need_future()
+
+  # After we're done getting the information, revert to the original plan
+  oplan <- plan()
+  on.exit(plan(oplan), add = TRUE)
+  # Change the plan
+  plan(list(
+    tweak(remote, workers = login_node),
+    tweak(remote, workers = check_node)
+  ))
+
+  node_text %<-% {
+    .keepme %<-% system("pbsnodes", intern = TRUE)
+    .keepme %>%
+      paste0(collapse="\n")
+  }
+
+  get_node_data(node_text)
+}
+
+#' Convert the output of `pbsnodes` into a list/data frame
+#'
+#' Runs `future`'s plan, in a way that works well with the Rochester cluster nodes
+#'
+#' @param info_string a single string of all the output of `pbsnodes`. Each line should be separated by a newline character
+#' @param as_df if `TRUE`, it returns it as a data frame, otherwise it returns it as a list
+#' @return A data frame / list with all the node information
+#' @export
+get_node_data <- function(info_string, as_df=TRUE) {
+  node_start <- "(?<=^|\\n)node[0-9]{2}\\.cs\\.rochester\\.edu"
+
+  node_names <- stringr::str_match_all(info_string, node_start) %>%
+    purrr::simplify()
+  node_info <- stringr::str_split(info_string, node_start) %>% purrr::simplify()
+  # Get rid of the first empty string
+  node_info <- node_info[2:length(node_info)]
+  node_list <- node_info %>%
+    # give each list a
+    purrr::set_names(node_names) %>%
+    purrr::map(function(info) {
+      l <- list(state = stringr::str_match(info, "(?<=state = ).+")[1])
+      status <- stringr::str_match(info, "(?<=status = ).+") %>%
+        stringr::str_split(",") %>% {
+          val_pairs <- purrr::map(., function(x) {
+            stringr::str_split(x,"=")}) %>%
+            pluck(1)
+          purrr::map(val_pairs, 2) %>%
+            set_names(purrr::map(val_pairs, 1))
+        }
+
+      if (!is.null(status) & length(status) > 1) { append(l, status) }
+      else {l}
+    })
+  if (as_df==TRUE) {
+    # Turn it into a data frame
+    imap_dfr(node_list, function(x, name) {
+      as.data.frame(x, stringsAsFactors=FALSE) %>%
+        mutate(node = name)
+    }) %>%
+      mutate(number = as.numeric(stringr::str_extract(node,"[0-9]+")))
+  } else {
+    node_list
+  }
+}
+
+
+
+#' Default functions to help determine which cluster node to use
+#'
+#' These functions just apply certain default assumptions to the node information
+#' data frames generated from `get_node_data`:
+#'
+#' * `default_cleanup` turns the important numeric values in the data frame (from `get_node_data`) from
+#'   character columns to actual numeric columns, removing the "kb" from the columns pertaining to memory,
+#'   and adding a column called `percent_free` which is the proportion of available memory out of the total memory.
+#' * `default_filter` simply removes nodes that aren't "free," have numbers greater than 64,
+#'   or have a proportion of free memory (`percent_free`) less than a certain threshold.
+#' * `get_n_best_nodes` returns `n` remaining nodes that have the most available memory.
+#'
+#' @param df a dataframe of node information, as produced by `get_node_data`
+#' @param threshold the threshold for the proportion of memory currently available by which to exclude nodes (e.g., 0.7 excludes nodes with less than 70\% of their memory currently available)
+#' @param n the number of nodes to select
+#' @export
+default_cleanup <- function(df) {
+  df %>%
+    mutate_at(vars(physmem:totmem),
+              ~sub("kb", "", .)) %>%
+    mutate_at(vars(loadave:nsessions), as.numeric) %>%
+    mutate(percent_free = availmem/totmem)
+}
+#' @rdname default_cleanup
+#' @export
+default_filter <- function(df,
+                           threshold = 0.7) {
+  df %>%
+    filter(state=="free",
+           number < 64,
+           percent_free >= threshold)
+}
+#' @rdname default_cleanup
+#' @export
+get_n_best_nodes <- function(df, n) {
+  nodes <- df %>%
+    arrange(-availmem) %>%
+    head(n)
+  if (nrow(nodes) < n) rlang::warn(paste0("Less than ", n, "nodes available (", nrow(nodes),")"))
+  nodes
+}
+
+
+name_if_possible <- function(x, nm = x, ...) {
+  len_diff <- length(x) - length(nm)
+  if (len_diff < 0) stop("`nm` cannot be LONGER than `x`")
+  nm <- append(nm, rep("", len_diff))
+  purrr::set_names(x, nm, ...)
+}
+
+.name_expander <- function(named_cores, ntabs=2) {
+  if (rlang::is_callable(named_cores)) return ("`function()`")
+  checker <- function(x) {
+    ifelse(rlang::is_character(x), paste0("\"", x, "\""),
+           ifelse(rlang::is_callable(x), "`function()`",
+                  paste0(x)))
+  }
+  tabs <- function(n) paste0(rep("  ", n), collapse="")
+
+  head <- paste0("function() {\n", tabs(ntabs), "switch(Sys.info()[[\"nodename\"]],\n")
+  body <- map(named_cores, checker) %>%
+    imap(function(x, i) {
+      if (i == "") return(paste0(tabs(ntabs+1), x))
+      else paste0(tabs(ntabs+1), "\"", i, "\" = ", x, ",")
+    }) %>%
+    reduce(~paste0(.x, "\n", .y))
+  tail <- paste0("\n", tabs(ntabs), ")\n", tabs(ntabs-1), "}")
+  paste0(head,body,tail)
+}
+
+
+
+#' Make a plan, given a set of nodes
+#'
+#' This is relatively complicated, because I made it needlessly flexible.
+#' Basically, it will either execute `future::plan` in a topology that
+#' logs into the login node, has a cluster set up with the nodes specified
+#' where on each of these nodes, it uses a `multiprocess`. The needlessly flexible
+#' bit is where I let the user specify the number of cores used for each cluster
+#' node either by a custom function or a list corresponding to the list of cluster
+#' hostnames (which can be numerics or functions).
+#'
+#' The bloat in this code comes from my desire to show the user what
+#' command might be run. Unfortunately, it's too much of a pain in the
+#'  butt to give users the proper amount of information about the
+#' functions they're calling while at the same time letting them
+#' customize those functions. Therefore, I represent all the arbitrary
+#' custom core-setting functions with \`function()\`.
+#'
+#' @param df a vector/list of node hostnames or a data frame with a column called "nodes" that has the same information
+#' @param login_node a string for the gateway login, e.g., \"zachburchill@cycle1.cs.rochester.edu\"
+#' @param core_mapper if `NULL`, the `multiprocess` plan uses its default arguments. If a function, `core_mapper` determines the number of cores used for each machine. If a list/vector, it will be assumed to be the number of cores for each particular node, in the same order as the nodes were given. `nodes_to_plan` will then convert this into a function automatically.
+#' @param goahead indicates whether this function should execute the plan or just return an expression representing the plan
+#' @param use_abbreviations because the way ssh adds known hosts, if you've only sshed into a node via `ssh nodeN`, you'll want to set this to `TRUE`
+#' @return Either nothing or an expression representing the structure of the plan.
+#' @export
+nodes_to_plan <- function(nodes, # the node df
+                          login_node, # the string for the remote worker
+                          core_mapper = NULL, # Not currently functional
+                          goahead = TRUE, # If true, goes ahead and runs the command.
+                          # If false, returns the expression
+                          use_abbreviations = TRUE # will attempt to ssh into "node33" rather than "node33.cs.rochester.edu"
+) {
+  if (is.data.frame(nodes)) nodes <- nodes$node
+
+  if (!is.null(core_mapper)) {
+    # If core_mapper is a function
+    if (rlang::is_callable(core_mapper)) {
+      customWorkers <- core_mapper
+      named_cores <- core_mapper
+      # If core_mapper is a (named) list of core numbers/core functions
+    } else if (rlang::is_list(core_mapper)) {
+      named_cores <- purrr::set_names(core_mapper, nodes)
+      customWorkers <- function() {
+        numcores <- do.call(switch, append(Sys.info()[["nodename"]], named_cores))
+        if (rlang::is_callable(numcores)) numcores()
+      }
+    } else {
+      stop(paste0("`core_mapper` must be a function or list, not ", typeof(core_mapper)))
+    }
+  }
+
+  if (use_abbreviations) nodes <- stringr::str_extract(nodes, "node[0-9]+")
+
+  head_s <- paste0("plan(list(
+  tweak(remote, workers = \"", login_node, "\"),
+  tweak(cluster, workers = c(\"", paste0(nodes, collapse="\", \""), "\")),
+  ")
+  if (is.null(core_mapper)) {
+    full_s <- paste0(head_s, "multiprocess\n))")
+  } else {
+    full_s <-paste0(head_s, "tweak(multiprocess, workers = ", .name_expander(named_cores), ")\n))")
+  }
+
+  message(paste0("Plan being executed (minus user-defined functions):\n", full_s))
+
+  if (goahead==TRUE) {
+    .need_future()
+    plan(list(
+      tweak(remote, workers = login_node),
+      tweak(cluster, workers = nodes),
+      multiprocess
+    ))
+    return_null <- NULL
+  } else {
+    return(rlang::parse_expr(full_s))
+  }
+}
+
+
+
+
